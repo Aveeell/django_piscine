ae5d376bfaacf5fc18798a4a32b37114b6918dd6
Jimmie Errok <jerrok@at-j4.msk.21-school.ru>
2022-11-10 12:25:20 +0300

    done 01 in 03

--- 03 - lib/ex01/my_program.py
+++ 03 - lib/ex01/my_program.py
@@ -1,1 +1,11 @@
-from os import path
+from path import Path
+
+if __name__ == '__main__':
+	try:
+		Path.makedirs('directory')
+	except FileExistsError as e:
+		print(e)
+	Path.touch('directory/file.txt')
+	f = Path('directory/file.txt')
+	f.write_lines(['hello world!'])
+	print(f.read_text())
--- 03 - lib/ex01/my_script.sh
+++ 03 - lib/ex01/my_script.sh
@@ -1,3 +1,11 @@
-#/bin/sh
+#!/bin/sh
 
-uname
+/usr/bin/python3 -m venv local_lib
+
+source local_lib/bin/activate
+
+python -m pip --version
+
+python -m pip install --log pip_install.log --force-reinstall git+https://github.com/jaraco/path.git
+
+python my_program.py
